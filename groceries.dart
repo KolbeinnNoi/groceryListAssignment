3524d6d9340a829412bceb72d2daca0ce04adf10
KolbeinnNoi <kolbeinnnoi99@gmail.com>
2024-10-10 22:28:53 +0000

    Made code more beautiful

--- groceries.dart
+++ groceries.dart
@@ -10,99 +10,17 @@ import 'dart:math';
 // Use Methods (functions) !!! <3
 
 List<String> groceries = [];
-// String addItemToList = stdin.readLineSync();
-// groceries.add(addItemToList);
 
-void main () {
-  print("Hi welcome to our grocery list app");
-  print("Press '1' if you would like to add an item to your grocery list");
-  print("Press '2' if you would like to view your list");
-  print("Press '3' to remove an item from the list!");
-  print("Press '4' if you would like to exit our app");
-
-  while(true) {
-  String? inputMenu = stdin.readLineSync();
-  int? menu = inputMenu != null ? int.tryParse(inputMenu) : null;
-  
-  // Bæta hlutum á listann
-  if(menu == 1) {
-  add();
-
-  } else if (menu == 2) {
-  view();
-  // --------------------------------------------------------------------------
-  // --------------------------------------------------------------------------
-  } else if (menu == 3) {
-  remove();
-  } else if (menu == 4) {
-  exit(0);
-  }else {
-    print("Your input: $menu is invalid, pick '1', '2' or '3' based on what you want to do");
-    continue;
-  } }
-}
-
-// Secondary menu if users have already used the list before
-void menu2 () {
-  print("-----------------------------------------------------------------------------");
-  print("Hi again, please choose one of the options below");
-  print("Press '1' if you would like to add an item to your grocery list");
-  print("Press '2' if you would like to view your list");
-  print("Press '3' to remove an item from the list!");
-  print("Press '4' if you would like to exit our app");
-
-  String? inputMenu = stdin.readLineSync();
-  int? menu = inputMenu != null ? int.tryParse(inputMenu) : null;
-
-  if(menu == 1) {
-  add();
-  }
-  else if (menu == 2) {
-  view();
-
-  } else if (menu == 3) {
-  remove();
-    
-  } else if (menu == 4) {
-  exit(0);
-  } else {
-  print("Your input: $menu is invalid, pick '1', '2' or '3' based on what you want to do");
-  }
-}
-void addItemToList (String item) {
-  if (item.isNotEmpty) {
-    String fixedItem = item[0].toUpperCase() + item.substring(1).toLowerCase();
-    groceries.add(fixedItem);
-    print("-----------------------------------------------------------------------------");
-    print("$item was added to your grocery list!");
-  } else {
-    print("Your input was invalid, please type in a valid item for your list or press '2' to go back to the menu");
-    // Láta notendur stimpla aftur inn hlut fyrir listann án þess að prenta út sama textann úr void add
-    String? input = stdin.readLineSync();
-    // ef þeir stimpla inn ekkert eða null value þá heldur while loopan í main functioni áfram
-    if (input == null || input.isEmpty) {
-      print("No input provided. Please enter a valid item or press '2' to go back to the menu.");
-      return;
-    }
-    String fixedItem2 = input[0].toUpperCase() + input.substring(1).toLowerCase();
-    if (input == '2') {
-      menu2();
-    } else if (input != null && input != '2' && input.isNotEmpty) {
-    String fixedItem2 = input[0].toUpperCase() + input.substring(1).toLowerCase();
-    addItemToList(fixedItem2);
-    } else {
-    print("$input is not a valid input, please type in a valid item or '2' to go back to the menu");
-  }
-    
-  }
-}
 
 void add () {
-  print("Please type in the item you want to add to your grocery list or press '2' to go back to then menu!"); 
+  print("Please type in the item you want to add to your grocery list or press '2' to go back to then menu!");
+  print("To add more than 1x of each item lets say for example 5 apples, type it like this '5x apples'.");
+  // Ég er með nokkur function sem eru að nota bool = true en vildi hafa eitt while (true). Engin ástæða tbh
   while(true) {
   String? input = stdin.readLineSync(); 
   if(input == '2') {
     menu2();
+    break;
   } else if (input != null && input != '2') {
     addItemToList(input);
   } 
@@ -134,8 +52,10 @@ void remove () {
   print("or, if you know the position of the item in the list, you can enter the corresponding number.");
   print("To go back to the menu, type in 'menu'");
   print("-----------------------------------------------------------------------------------------------------");
-  while(true) {
+  bool removeFunction = true;
+  while(removeFunction) {
   print("Here is your grocery list!");
+  // i stækkar frá 0 upp í hvað sem listinn er langur og hækkar um 1 fyrir hvert item og þá prentast út [1]. fyrsti hlutur  [2]. annar hlutur  etc...
   for(int i = 0; i < groceries.length; i++) {
   print("${[i + 1]}. ${groceries[i]}");
   }
@@ -144,7 +64,7 @@ void remove () {
   int? location = removeInput != null ? int.tryParse(removeInput) : null;
   // Checka hvort input sé tala eða string og ef það er tala sjá til þess að talan sé í takti við rangeið á listanum. Byrja listann á 1 svo hann sé notendavænni
   if (location != null && location > 0 && location <= groceries.length) {
-    // gerum -1 svo ef notandi ætlar að velja fyrsta hlut í listanum þá fær hann actually fyrsta hlutinn við að skrifa ´1´ í staðinn fyrir '0'
+  // gerum -1 svo ef notandi ætlar að velja fyrsta hlut í listanum þá fær hann actually fyrsta hlutinn við að skrifa ´1´ í staðinn fyrir '0'
   String removeLocation = groceries.removeAt(location - 1);
   print("$removeLocation has been removed from the list");
   } else if (removeInput != null && groceries.contains(removeInput)) {
@@ -152,13 +72,116 @@ void remove () {
     print("$removeInput has been removed from the list");
   } else if (removeInput == "menu") {
     menu2();
+    removeFunction = false;
   }else {
     print("Your input was invalid please try again!");
-    continue;
   }
     
 } 
 }
+
+
+void menuText () {
+  print("Press '1' if you would like to add an item to your grocery list");
+  print("Press '2' if you would like to view your list");
+  print("Press '3' to remove an item from the list!");
+  print("Press '4' if you would like to exit our app");
+}
+
+// Set þetta fyrir utan functionið svo þetta virki líka í menu2()
+// Gat lika notað exit(0); en vildi prufa þetta
+bool mainFunction = true;
+void main () {
+  print("Hi welcome to our grocery list app");
+  menuText();
+  
+
+  
+  while(mainFunction) {
+  String? inputMenu = stdin.readLineSync();
+  int? menu = inputMenu != null ? int.tryParse(inputMenu) : null;
+  
+  // Switch statement var meira clean hérna en if og else
+  
+  switch (menu) {
+    case 1:
+      add();
+      break;
+    case 2:
+      view();
+      break;
+    case 3:
+      remove();
+    case 4:
+      print("Exiting the app..");
+      mainFunction = false;
+      break;
+    default:
+      print("Invalid input... please select '1', '2', '3' or '4'.");  
+  
+    }
+  }
+}
+
+void menu2 () {
+  print("-----------------------------------------------------------------------------");
+  print("Hi again, please choose one of the options below");
+  menuText();
+
+
+  String? inputMenu = stdin.readLineSync();
+  int? menu = inputMenu != null ? int.tryParse(inputMenu) : null;
+
+  // Einfoldum þetta með switch statement líka
+  switch (menu) {
+    case 1:
+      add();
+      break;
+    case 2:
+      view();
+      break;
+    case 3:
+      remove();
+    case 4:
+      print("Exiting the app..");
+      mainFunction = false;
+      break;
+    default:
+      print("Invalid input... please select '1', '2', '3' or '4'.");  
+  
+    }
+}
+
+void addItemToList (String item) {
+  if (item.isNotEmpty) {
+    // Þetta lætur fyrsta stafinn verða að stórum staf og restin af stöfunum verða litlir
+    String fixedItem = item[0].toUpperCase() + item.substring(1).toLowerCase();
+    groceries.add(fixedItem);
+    print("-----------------------------------------------------------------------------");
+    print("You have added $item to your list");
+  } else {
+    print("Your input was invalid, please type in a valid item for your list or press '2' to go back to the menu");
+    // Láta notendur stimpla aftur inn hlut fyrir listann án þess að prenta út sama textann úr void add
+    String? input = stdin.readLineSync();
+    // ef þeir stimpla inn ekkert eða null value þá heldur while loopan í main functioni áfram
+    if (input == null || input.isEmpty) {
+      print("No input provided.");
+      return;
+    }
+    String fixedItem2 = input[0].toUpperCase() + input.substring(1).toLowerCase();
+    if (input == '2') {
+      menu2();
+    } else if (input != null && input != '2' && input.isNotEmpty) {
+    String fixedItem2 = input[0].toUpperCase() + input.substring(1).toLowerCase();
+    addItemToList(fixedItem2);
+    } else {
+    print("$input is not a valid input, please type in a valid item or '2' to go back to the menu");
+  }
+    
+  }
+}
+
+
 
 
 
